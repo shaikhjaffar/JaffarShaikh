ab46b193071e80357dba18e6e0ee0704c3264a8f
[firstname <jaffar.shaikh@1clickcapital.com>
2023-07-16 23:40:38 +0530

    added

--- src/Experience/Utils/assets.js
+++ src/Experience/Utils/assets.js
@@ -4,11 +4,11 @@ export default [
     {
         name: "room",
         type: "glbModel",
-        path: "./models/Finale Version 16.glb",
+        path: "/models/Finale Version 16.glb",
     },
     {
         name: "screen",
         type: "videoTexture",
-        path: "./textures/kda.mp4",
+        path: "/textures/kda.mp4",
     },
 ];
